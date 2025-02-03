75dfc026f8e16b3deda4faabc2b363a5604d4047
max12525k <62839496+max12525k@users.noreply.github.com>
2025-02-03 23:15:09 +0530

    custom.el file was added

---
+++ custom.el
@@ -0,0 +1,15 @@
+;;; custom.el -*- lexical-binding: t; -*-
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(codeium/metadata/api_key "9f154300-577a-4f8d-82cb-e9a2cd4c003a"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange")))))
